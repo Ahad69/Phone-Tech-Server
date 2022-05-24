12de53a1431bf7785274b51ad7ef336bce4cd232
Ahad hossain <ahadxx99@gmail.com>
2022-05-24 04:55:43 -0500

    admin added

--- index.js
+++ index.js
@@ -16,6 +16,22 @@ app.use(express.json())
 const uri = `mongodb+srv://${process.env.USER_NAME}:${process.env.USER_PASS}@cluster0.sbmqf.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
+
+function verifyToken(req , res , next){
+  const authHeader = req.headers.authorization;
+  if(!authHeader){
+    return res.status(401).send({message : 'unauthorised'})
+  }
+  const token = authHeader.split(' ')[1]
+  jwt.verify(token , process.env.TOKEN , (err , decoded)=>{
+    if(err){
+      return res.status(403).send({message : 'Forbidden'})
+    }
+    req.decoded = decoded;
+  })
+  next()
+}
+
 async function run() {
     try {
       await client.connect();
@@ -54,11 +70,19 @@ async function run() {
       })
 
       // get orders by email 
-      app.get('/orders', async(req,res)=>{
+      app.get('/orders', verifyToken , async(req,res)=>{
         const customerEmail = req.query.customerEmail;
-         const query = {customerEmail}
-        const result = await orderCollection.find(query).toArray()
-        res.send(result)
+         const decodedEmail =  req.decoded.email;
+
+         if(customerEmail == decodedEmail){
+          const query = {customerEmail}
+          const result = await orderCollection.find(query).toArray()
+          res.send(result)
+         }
+         else{
+           return res.status(403).send({ message : "forbidden access"})
+         }
+       
       })
 
       // delete orders 
@@ -97,6 +121,24 @@ async function run() {
         res.send(result)
       })
 
+      app.get('/users' , verifyToken,   async(req , res)=>{
+        const query = {}
+        const result = await userCollection.find(query).toArray()
+        res.send(result)
+      });
+      app.put('/users/admin/:email' , verifyToken , async(req , res)=>{
+        const email = req.params.email;
+        const requester = req.decoded.email
+        console.log(requester)
+        const filter = {email : email};
+        const updateDocs = {
+          $set : {role : 'admin'}
+        };
+        const result = await userCollection.updateOne(filter  , updateDocs );
+        console.log(email , filter , updateDocs)
+        res.send(result)
+      })
+
       app.put('/users/:email' , async(req , res)=>{
         const email = req.params.email;
         const user = req.body;
@@ -109,6 +151,20 @@ async function run() {
         const token = jwt.sign({email : email}, process.env.TOKEN , {expiresIn : '1d'})
         res.send({ result , token})
       })
+
+      app.delete('/users/:id' , async(req,res)=>{
+        const id = req.params.id;
+        const query = {_id:ObjectId(id)}
+        const result = await userCollection.deleteOne(query);
+        res.send(result)
+    })
+
+    app.get('/admin/:email' , async(req , res)=>{
+      const email = req.params.email;
+      const user = await userCollection.findOne({email : email});
+      const isAdmin = user.role === 'admin' ;
+      res.send(isAdmin)
+    })
       
     } finally {
     //   await client.close();
