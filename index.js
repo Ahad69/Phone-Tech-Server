61b2b80382d930b252d2ef66e4f8c0898fdf28ee
Ahad hossain <ahadxx99@gmail.com>
2022-05-24 01:45:40 -0500

    user creation

--- index.js
+++ index.js
@@ -23,24 +23,37 @@ async function run() {
       const orderCollection = client.db("phoneTech").collection("orders");
       const reviewsCollection = client.db("phoneTech").collection("reviews");
       const profileCollection = client.db("phoneTech").collection("profile");
-    
+      const userCollection = client.db("phoneTech").collection("users");
+     
+      // get products
       app.get('/products' , async(req, res)=>{
           const query = {};
           const result = await productsCollection.find(query).toArray()
           res.send(result)
       })
 
+      // post products 
+      app.post('/products' , async(req , res)=>{
+        const query = req.body;
+        const result = await productsCollection.insertOne(query)
+        res.send(result)
+      })
+
+      // post orders 
       app.post('/orders' , async(req,res)=>{
         const query = req.body;
         const result = await orderCollection.insertOne(query);
         res.send(result)
       })
+
+      // get all-orders
       app.get('/all-orders', async(req,res)=>{
         const query = {}
         const result = await orderCollection.find(query).toArray()
         res.send(result)
       })
 
+      // get orders by email 
       app.get('/orders', async(req,res)=>{
         const customerEmail = req.query.customerEmail;
          const query = {customerEmail}
@@ -48,6 +61,7 @@ async function run() {
         res.send(result)
       })
 
+      // delete orders 
       app.delete('/orders/:id' , async(req,res)=>{
           const id = req.params.id;
           const query = {_id:ObjectId(id)}
@@ -55,27 +69,45 @@ async function run() {
           res.send(result)
       })
     
+      // post reviews
       app.post('/reviews' , async(req,res)=>{
         const query = req.body;
         const result = await reviewsCollection.insertOne(query);
         res.send(result)
       })
+
+      // get reviews
       app.get('/reviews', async(req,res)=>{
         const query = {};
         const result = await reviewsCollection.find(query).toArray()
         res.send(result)
       })
-
+      // post profile 
       app.post('/profile' , async(req , res)=>{
         const query = req.body;
         const result = await profileCollection.insertOne(query);
         res.send(result)
       })
+
+      // get profile 
       app.get('/profile' , async(req , res)=>{
         const userEmail = req.query.userEmail;
         const query = {userEmail}
         const result = await profileCollection.find(query).toArray();
         res.send(result)
+      })
+
+      app.put('/users/:email' , async(req , res)=>{
+        const email = req.params.email;
+        const user = req.body;
+        const filter = {email : email};
+        const options = { upsert : true};
+        const updateDocs = {
+          $set : user
+        };
+        const result = await userCollection.updateOne(filter  , updateDocs , options);
+        const token = jwt.sign({email : email}, process.env.TOKEN , {expiresIn : '1d'})
+        res.send({ result , token})
       })
       
     } finally {
