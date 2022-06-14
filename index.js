6b116784485d65434ddbb660b739d84ac3f6805c
Ahad hossain <ahadxx99@gmail.com>
2022-06-14 01:40:21 -0500

    heroku

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ const stripe = require('stripe')(process.env.STRIPE_KEY)
 // app.options('*', cors(corsConfig))
 
 // app.use(cors({origin : 'https://assignment-12-42953.web.app'}))
-app.use(cors({}))
+app.use(cors())
 app.use(express.json())
 // app.use('*', cors())
 // const corsConfig = {
